631c30b52204590ee25184751d00dcefc7ebcc53
Gülay AKTAŞ <58680788+Gulay-aktas@users.noreply.github.com>
2021-10-29 18:34:10 +0300

    Add files via upload

---
+++ 2_hafta/hello_world.java/src/hello_world/java/hello_world.java
@@ -0,0 +1,10 @@
+package hello_world.java;
+
+public class hello_world {
+
+	public static void main(String[] args) { //public eriþime açýk olduðunu gösterir,void deðer döndürmeyen parametredir.
+		  System.out.println("MERHABA DÜNYA"); // ekrana yazdýrma komutu
+
+	}
+
+}
